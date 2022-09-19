935084d5c944b59f34e5c26dc172486a5c42df60
OlgaLeb21 <103959901+OlgaLeb21@users.noreply.github.com>
2022-09-19 15:25:51 +0300

    Input and output

--- Ex000/Program.cs
+++ Ex000/Program.cs
@@ -3,3 +3,17 @@ int n = Convert.ToInt32(Console.ReadLine());
 
 string[] OldArray = new string[n];
 string[] NewArray = new string[n];
+
+for (int i = 0; i < n; i++)
+{
+    OldArray[i] = Console.ReadLine();
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < n; i++)
+{   
+    Console.WriteLine($"String {i + 1} = {OldArray[i]}");
+}
+
+Console.WriteLine();
