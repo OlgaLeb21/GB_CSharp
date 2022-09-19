19d2bba8af6b1f9f95dae3288bf7add78e999872
OlgaLeb21 <103959901+OlgaLeb21@users.noreply.github.com>
2022-09-19 15:28:08 +0300

    Swap

--- Ex000/Program.cs
+++ Ex000/Program.cs
@@ -17,3 +17,56 @@ for (int i = 0; i < n; i++)
 }
 
 Console.WriteLine();
+Console.Write("Enter a character limit: ");
+int k = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+for (int i = 0; i < n; i++)
+{
+    int count = 0;
+    for (int j = 0; j < OldArray[i].Length; j++)
+    {
+        count += 1;
+    }
+    if (count > k) 
+    {
+        OldArray[i] = "";
+    }
+}
+
+for (int i = 0; i < n - 1; i++) 
+{   
+    for (int j = 0; j < n - 1; j++) 
+    {
+        if (OldArray[j] == string.Empty) 
+        {
+            string temp = OldArray[j];
+            OldArray[j] = OldArray[j+1];
+            OldArray[j+1] = temp;
+        }
+    }    
+}
+
+for (int i = 0; i < n; i++)
+{   
+    Console.WriteLine($"String {i + 1} = {OldArray[i]}");
+}
+
+int countElements = 0;
+for (int i = 0; i < n; i++)
+{   
+    if (OldArray[i] != string.Empty) 
+    {
+        countElements += 1;
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine($"The new array will have {countElements} elements");
+
+for (int i = 0; i < countElements; i++) 
+{
+    NewArray[i] = OldArray[i];
+}
+
+Console.WriteLine();
