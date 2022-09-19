ee83232bbabd49aac61bff02dba8298182869ff5
OlgaLeb21 <103959901+OlgaLeb21@users.noreply.github.com>
2022-09-19 15:24:38 +0300

    Step1

--- Ex000/Program.cs
+++ Ex000/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Enter the number of lines: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string[] OldArray = new string[n];
+string[] NewArray = new string[n];
